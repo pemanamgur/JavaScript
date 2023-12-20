52ea95f2c9ce30ccb95785833c6f424230d92e48
pemanamgur <152847625+pemanamgur@users.noreply.github.com>
2023-12-20 13:26:56 +0545

    Add files via upload

---
+++ Java Script/Basic/08DateAndTime.js
@@ -0,0 +1,22 @@
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toUTCString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toISOString())
+// console.log(typeof myDate);
+
+// let myNewDate = new Date(2023,12,14); //month 0 bata suru huncha so 12 lekda jan vancha
+let myNewDate = new Date(2023,12,14,6,59); 
+let myNewDates = new Date("2023-12-12"); 
+// console.log(myNewDates);
+// console.log(myNewDate.toDateString());
+// console.log(myNewDate.toLocaleString());
+
+let d = new Date(); 
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+
+
