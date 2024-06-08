12037c01bee0023a0dba86f33bd22fbe5001c41a
Hello World <152847625+pemanamgur@users.noreply.github.com>
2024-06-08 13:30:26 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
+//plaindrome
+const arr = ["1", "2", "3"];
+// console.log(arr.toString());
+console.log(arr.reverse());
+let newStr = arr.join("");
+console.log(newStr);
 
+//callback
+function abc(a, b) {}
 
-// const user = {
-//     username : "Pema",
-//     loginCount : 8,
-//     signedIn : true,
-//     userDetail : function(){
-//         // console.log(this)
-//         // console.log(`My name is Nabin`);
-//         // console.log(`Username : ${this.username}`)
-//         console.log(this);
-//     }
-// }
+abc("1", () => {});
 
-// console.log(user.username);
-// // console.log(user.userDetail());
-// console.log(this);
-
-
-function MyInfo(username,userAge,userEmail){
-     this.username = username;
-     this.userAge = userAge;
-     this.userEmail = userEmail;
-     this.greeting = function(){
-        console.log(`Hello world , My name is ${this.username}`);
-     }
-
-     return this;
-}
-
-const user1 = new MyInfo("Pema" , 22 , 'pemanamgurtamang7@gmail.com');
-const user2 = new MyInfo("Tema" , 22 , 'pemanamgurtamang7@gmail.com');
-// console.log(user1);
-// console.log(user1.greeting());
-// console.log(user1.constructor);
-// console.log(user2.constructor);
-// console.log(user2.greeting());
-
-
-// console.log(user1 instanceof MyInfo);
-// console.log(user2 instanceof MyInfo);
-// console.log(Pema instanceof MyInfo);
+//higher oreder function accepts callback
+//json client server   => there is no function
+//object bitra function is method.
+//function is independent and mehtod is built in .
+//V-dom real dom change
+//js is a loosely typed language  and single thread;
+//array , object , maps , hashmap;
+//patch and options also options -> prefied request   patch->update garnu banda agadi Put->update
+//get bata update garnu milcha tara security .
+//axios and fetch
